7be105438d9093a3c4ef0fa752960e3ceeeb5321
Fernando Hanz Hernandez De Leon <hanzherdel@gmail.com>
2019-12-15 22:03:22 -0600

    --

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,27 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+import { MqttModule, MqttService , IMqttServiceOptions} from 'ngx-mqtt';
+
+import {NgxChildProcessModule} from 'ngx-childprocess';
+import {ChildProcessService} from 'ngx-childprocess';
+
+export const MQTT_SERVICE_OPTIONS : IMqttServiceOptions = {
+  /*hostname: '192.168.1.4',
+  protocol : "ws",
+  port: 8883,
+*/
+  hostname: 'test.mosquitto.org',
+  port: 8080,
+};
+export function mqttServiceFactory() {
+  return new MqttService(MQTT_SERVICE_OPTIONS);
+}
+
 
 @NgModule({
   declarations: [
@@ -14,7 +32,13 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    MqttModule.forRoot({
+      provide: MqttService,
+      useFactory: mqttServiceFactory,
+      NgxChildProcessModule,
+      ChildProcessService,
+    }),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,119 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { SamsungTvRemote } from 'samsung-tv-remote';
+import { Injectable } from '@angular/core';
+import { MqttService, IMqttMessage } from 'ngx-mqtt';
+import { Subscription } from 'rxjs'
+//import {ChildProcessService} from 'ngx-childprocess';
 
+
+const WebSocket = require('ws');
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ subs : Subscription;
+  message : string = "";
+  topic : string = "";
+  author : string = "app : ";
+  publishM : string = "";
+  messageArray = [];
+  temp=20;
+  keyControl:string='KEY_MENU';
+ // ws = new WebSocket('ws://echo.websocket.org');
+config = {
+   ip: '192.168.5.31' ,// required: IP address of your Samsung Smart TV
+     name :  '[Signage] Display 1',
+     mac :  '7C:64:56:A1:F2:F8',
+     port :  8001,
+  timeout :  5000,
+};
+base64Encode = function(string) {
+  return new Buffer(string).toString('base64');
+};
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private mqttService: MqttService) {
+ 
+ // this.sendKey();
+  }
+  sendKey() {
+    let key=this.keyControl;
+    console.log(key);
+    if( key ) {
+      const url = `ws://${this.config.ip}:${this.config.port}/api/v2/channels/samsung.remote.control?name=${this.base64Encode(this.config.name)}`;
+      let ws = new WebSocket(url, (error) => {
+        console.log(new Error(error));
+      });
+      ws.on('error', (error) => {
+        console.log(`Samsung Remote Client:${ error}`);
+      });
+      ws.on('message', (data, flags) => {
+        data = JSON.parse(data);
+        if( data.event === 'ms.channel.connect' ) {
+          ws.send(JSON.stringify({
+            'method': 'ms.remote.control',
+            'params': {
+              'Cmd': 'Click',
+              'DataOfCmd': key,
+              'Option': 'false',
+              'TypeOfRemote': 'SendRemoteKey'
+            }
+          }));
+          setTimeout(() => {
+            ws.close();
+          }, 1000);
+        }
+      });
+    }
+  };
+/*isTvAlive (done) {
+    return this._childProcessService.childProcess.exec('ping -c 1 ' + this.config.ip,[], (error, stdout, stderr) => {
+            done(error ? false : true);
+        });
+  };*/
+  apagar(){
+  	this.publish("reuniones","APAGAR");
+  }
+  encender(){
+  	this.publish("reuniones","ENCENDER");
+  }
+  autoOn(){
+  	this.publish("reuniones","MODO AUTO");
+  }
+  autoOff(){
+  	this.publish("reuniones","MODO MANUAL");
+  }
+  cambiar(val){
+    this.publish("tv",val);
+  }
+  ambientador(val){
+    this.publish("ambient",val);
+  }
+  modificarTemp(){
+     this.publish("ambient",this.temp.toString());
   }
 
+   subscribe( topic : string){
+    if(this.subs){
+      this.unsubscribe();
+    }
+    this.messageArray = [];
+
+    this.subs = this.mqttService.observe(topic).subscribe(
+      (message : IMqttMessage) => {
+        console.log(message);
+        this.message = message.payload.toString();
+        this.messageArray.push(this.message);
+      });
+
+  }
+  publish(topic:string, publishM:any) {
+    console.log(topic);
+    this.mqttService.unsafePublish(topic, publishM);
+  }
+  unsubscribe(){
+    console.log("unsubscribe");
+    this.subs.unsubscribe();
+  }
 }
